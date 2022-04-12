567d292ba45d737a253d79e413b784ab87688b3e
Antonio Ribeiro <up201906761@up.pt>
2022-04-13 00:25:49 +0100

    added error handling to simple parser

--- src/pt/up/fe/comp/SimpleParser.java
+++ src/pt/up/fe/comp/SimpleParser.java
@@ -51,8 +51,17 @@ public class SimpleParser implements JmmParser {
 
             return new JmmParserResult((JmmNode) root, Collections.emptyList(), config);
 
-        } catch (Exception e) {
-            return JmmParserResult.newError(Report.newError(Stage.SYNTATIC, -1, -1, "Exception during parsing", e));
+        } catch (Exception ex) {
+            var e = TestUtils.getException(ex, ParseException.class);
+
+            // ... test if ‘e’ is null and handle ‘ex’ in a more generic way
+            assert e != null;
+            Token t = e.getToken();
+            int line = t.getBeginLine();
+            int column = t.getBeginColumn();
+            String message = e.getMessage();
+            Report report = Report.newError(Stage.SYNTATIC, line, column, message, e);
+            return JmmParserResult.newError(report);
         }
     }
 }
