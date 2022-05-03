cf022377b6d3abebd8a614e0aa601408e8220aab
Antonio Ribeiro <up201906761@up.pt>
2022-05-03 23:32:02 +0100

    added basic analysis test template

---
+++ test/AnalysisTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import pt.up.fe.comp.TestUtils;
+import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.specs.util.SpecsIo;
+
+public class AnalysisTest {
+
+    @Test
+    public void test() {
+        JmmSemanticsResult result = TestUtils.analyse(SpecsIo.getResource("fixtures/public/HelloWorld.jmm"));
+        System.out.println("Symbol Table: " + result.getSymbolTable());
+    }
+}
