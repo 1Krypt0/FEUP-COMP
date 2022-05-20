19e9c1c17d9ce639ebd7fcc81e3fb038a3849c2c
Mário Travassos <up201905871@fe.up.pt>
2022-05-20 23:42:27 +0100

    feat: add simple jasmin test

--- test/pt/up/fe/comp/JasminTest.java
+++ test/pt/up/fe/comp/JasminTest.java
@@ -1,13 +1,16 @@
 package pt.up.fe.comp;
 
 import org.junit.Test;
+import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.specs.util.SpecsIo;
+
+import java.util.Collections;
 
 public class JasminTest {
     @Test
     public void test(){
-        var jasminResult = TestUtils.backend(".class public HelloWorld\n");
-        TestUtils.noErrors(jasminResult);
-        //String result = jasminResult.compile();
+        var ollirResult = new OllirResult(SpecsIo.getResource("fixtures/public/cp2/OllirToJasminBasic.ollir"), Collections.emptyMap());
+        var jasminResult = TestUtils.backend(ollirResult);
     }
+
 }
