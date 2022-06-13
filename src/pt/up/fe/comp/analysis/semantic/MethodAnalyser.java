fa4b0d3970950da4110492477a2a09b972e3d0be
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-06-13 09:38:06 +0100

    Change semantic analysis structure

---
+++ src/pt/up/fe/comp/analysis/semantic/MethodAnalyser.java
@@ -0,0 +1,9 @@
+package pt.up.fe.comp.analysis.semantic.methods;
+
+import pt.up.fe.comp.jmm.ast.PreorderJmmVisitor;
+import pt.up.fe.comp.jmm.report.Report;
+
+import java.util.List;
+
+public class MethodAnalyser extends PreorderJmmVisitor<List<Report>, String> {
+}
